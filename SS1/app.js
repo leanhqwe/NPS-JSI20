bececc027230a80949a6275bce2d1f2a43ee72e4
leanhqwe <leanhsasuke0@gmail.com>
2024-03-24 17:29:25 +0700

    SS1

---
+++ SS1/app.js
@@ -0,0 +1,16 @@
+// cho 1 dữ liệu sau 
+let student = [{
+    id: 1,
+    name: "Le Anh",
+    age: 1,
+    major: "computer sience",
+},
+{
+    id: 2,
+    name: "Vu DUng",
+    age: 12,
+    major: "mathematics",
+},
+];
+
+student.push()
