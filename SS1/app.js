2ff0051c6e224ea590798e98647f09c4795c8e8f
leanhqwe <leanhsasuke0@gmail.com>
2024-03-24 18:01:13 +0700

    ss1

--- SS1/app.js
+++ SS1/app.js
@@ -13,4 +13,20 @@ let student = [{
 },
 ];
 
-student.push()
+student.push({id:"3", name:"Quan", age:"11", major:"idk",
+});
+console.log(student);
+console.log(student.find(Obj => Obj.id ==2));
+
+student.splice(1,1)
+
+
+
+
+/*
+yêu cầu :
+sử dụng push()để thêm một học sinh mới vào
+sử dụng find() hoặc findindex() để tìm học sinh theo id,
+sử dụng splice() để xóa một học sinh dựa trên id
+tạo 1 function cho phép tìm kiếm học sinh dựa theo name hoặc chuyên ngành
+*/
